60e83a815d1121a948eb946cf8a934897eb2731e
qiugh <graphy.qiu@gmail.com>
2018-03-02 14:31:02 +0800

    jd&tmall cal

--- process.js
+++ process.js
@@ -1,51 +1,104 @@
 let fs = require('fs');
 let moment = require('moment');
-let Status = require('./status.js');
 
 let userFile = '', endDate = '';
-let recordsFileName = ['jdRecordsFile.csv', 'tmRecordFile.csv'];
-let platforms = ['jd', 'tm'], startDate = '2010-01-01', recIdAttr = ['dealTime', 'orderId'];
+let recordsFileName = ['jdRecordsFile.csv', 'tmRecordFile.csv'];////do not disrupt the order
+//params above change everytime and need to be filled before starting program
+
+let statusSet = initStatus();
+let types = ['all', 'both', 'filterVirtual'];
+
+let startDate = '2010-01-01';
+let platforms = ['jd', 'tm'],
+    jdUserSet = {}, tmUserSet = {},
+    recIdAttr = ['dealTime', 'orderId'],
+    userSet = { 0: jdUserSet, 1: tmUserSet },
+    recsName = { 0: 'jdRecs', 1: 'tmRecs' };
+
+let userAttrisValueSet = [{}, {}, {}, {}, {}];
 let userAttris = ['age', 'city', 'edu', 'income', 'gender'];
-let userAttrisValue = [{}, {}, {}, {}, {}];
 let userFileHead = 'id,tmSuccess,jdSuccess,city,gender,age,edu,netAge,income,tmId,jdId'.split(',');
+
 let recordAttris = [
     'orderId,price,paidWay,status,dealTime,saler,productNumber,productName,productId,productUrl,loginName,cate'.split(','),
     'orderId,price,postageFee,status,payInfo,dealTime,productNumber,productName,skuId,productUrl,productPrice,productRealPrice,productQuantity,shopName,shopUrl,loginName,cate'.split(',')
 ];
-
-let params = process.argv.splice(2);
-let timeUnitNum = params[2], getTimeUnit;
-let jdUserSet = {}, tmUserSet = {},
-    userSet = { 0: jdUserSet, 1: tmUserSet },
-    recsName = { 0: 'jdRecs', 1: 'tmRecs' };
+let timeUnitNum = process.argv[2], getTimeUnit;
+let target = ['userNum', 'cost', 'buyFrequence', 'cateNum'];
 
 init();
 
-let tmAllUsers = getTmAllUsers();
-let tmBothUsers = getTmBothUsers();
-let tmFilterVirtualUsers = getTmFilterVirtualUsers();
-let [timeUnits, retention] = calUserRetention();
+calUsers(0, 0);
+calUsers(0, 1);
+calUsers(0, 2);
+calUsers(1, 0);
+calUsers(1, 1);
+calUsers(1, 2);
 
-function overall(tus, ret, pfIdx) {
-    for (let i = 0; i < tus.length; i++) {
-        for (let j = 0; j < tus.length; j++) {
-            let rst = stat(Object.keys(ret[i + '_' + j]), tus[j], pfIdx);
-        }
+function calUsers(pfIdx, type) {
+    let userIds0;
+    if (type == 0) {
+        userIds0 = getAllUsers(pfIdx);
+    } else if (type == 1) {
+        userIds0 = getBothUsers(pfIdx);
+    } else {
+        userIds0 = getFilterVirtualUsers(pfIdx);
     }
+    let [timeUnits0, retention0] = calUserRetention(userIds0, pfIdx);
+
+    let title = '\ufeff' + timeUnits0[0];
+    for (let i = 1; i < timeUnits0.length; i++) {
+        title = title + ',' + timeUnits0[i];
+    }
+    title = title + '\n';
+    calTarget(title, timeUnits0, retention0, pfIdx, type);
+    breakdown(title, timeUnits0, retention0, pfIdx, type);
 }
 
-function breakdown(tus, ret, pfIdx) {
-    for (let l = 0; l < userAttris.length; l++) {
-        let values = userAttrisValue[l];
-        for (let v = 0; v < values.length; v++) {
-            for (let i = 0; i < tus.length; i++) {
-                for (let j = 0; j < tus.length; j++) {
-                    let ids = Object.keys(ret[i + '_' + j]).filter(uid => {
-                        return userSet[pfIdx][uid][userAttris[l]] == values[v];
-                    });
-                    let rst = stat(ids, tus[j], pfIdx);
-                }
+function init() {
+    if (timeUnitNum == 0) {
+        getTimeUnit = (month => month > 6 ? 2 : 1);//halfyear
+    } else {
+        getTimeUnit = (month => Math.ceil(month / 3));//quarter
+    }
+    getUsers();
+    getRecords(0);
+    getRecords(1);
+}
+
+function calTarget(title, tus, ret, pfIdx, type, flag, attriName, attriValue) {
+    let content = target.map(() => title);
+    let files = target.map(tar => platforms[pfIdx] + '_' + flag + '_' + types[type] + '_' + tar + '.csv');
+    if (attriName !== undefined) {
+        files = target.map(tar => platforms[pfIdx] + '_' + flag + '_' + types[type] + '_' + tar + '_' + attriName + '_' + attriValue + '.csv');
+    }
+    for (let i = 0; i < tus.length; i++) {
+        let rst = target.map(() => tus[i]);
+        for (let j = 0; j < tus.length; j++) {
+            let ids = Object.keys(ret[i + '_' + j]);
+            if (flag == 'breakdown') {
+                ids = ids.filter(uid => {
+                    return userSet[pfIdx][uid][attriName] == attriValue;
+                });
             }
+            let statRst = stat(ids, tus[j], pfIdx);
+            target.forEach((name, idx) => {
+                rst[idx] = rst[idx] + ',' + statRst[name];
+            });
+        }
+        rst = rst.map(str => srt + '\n');
+        content = content.map((ori, idx) => ori + rst[idx]);
+    }
+    content.forEach((rstStr, idx) => {
+        fs.appendFileSync(files[idx], rstStr);
+    });
+}
+
+function breakdown(title, tus, ret, pfIdx, type) {
+    for (let l = 0; l < userAttris.length; l++) {
+        let values = userAttrisValueSet[l];
+        for (let v = 0; v < values.length; v++) {
+            calTarget(title, tus, ret, pfIdx, type, 'breakdown', userAttris[l], values[v]);
         }
     }
 }
@@ -111,16 +164,6 @@ function calUserRetention(userIds, pfIdx) {
 }
 
 
-function init() {
-    if (timeUnitNum == 0) {
-        getTimeUnit = (month => month > 6 ? 2 : 1);//halfyear
-    } else {
-        getTimeUnit = (month => Math.ceil(month / 3));//quarter
-    }
-    getUsers();
-    getRecords(0);
-    getRecords(1);
-}
 
 function getUsers() {
     fs.readFileSync(userFile).toString().trim().split('\n').map(line => {
@@ -137,7 +180,7 @@ function getUsers() {
             jdUserSet[user.jdId] = user;
         }
     });
-    userAttrisValue = userAttrisValue.map(valueSet => Object.keys(valueSet));
+    userAttrisValueSet = userAttrisValueSet.map(valueSet => Object.keys(valueSet));
 }
 
 function preprocessUser(user) {
@@ -150,7 +193,7 @@ function preprocessUser(user) {
     user.jdRecs = [];
     user.tmRecs = [];
     for (let i = 0; i < userAttris.length; i++) {
-        userAttrisValue[i][user[userAttris[i]]] = 1;
+        userAttrisValueSet[i][user[userAttris[i]]] = 1;
     }
 }
 
@@ -211,7 +254,7 @@ function getFilterVirtualUsers(pfIdx) {
 }
 
 function recValid(platform, rec) {
-    let status = Status.getStatus(platform, rec.status);
+    let status = getStatus(platform, rec.status);
     if (status != 'Successful')
         return false;
     let dealTime = moment(rec.dealTime);
@@ -255,4 +298,82 @@ function assign(obj, attris, data) {
         obj[attri] = {};
     }
     assign(obj[attri], attris, data);
+}
+
+function initStatus() {
+    let jdstatus = {
+        已完成: 'Successful',
+        出票成功: 'Successful',
+        等待收货: 'Successful',
+        充值成功: 'Successful',
+        '已完成</br>(充值成功)': 'Successful',
+        正在出库: 'Successful',
+        商品出库: 'Successful',
+        等待处理: 'Successful',
+        等待厂商处理: 'Successful',
+        正在处理: 'Successful',
+        缴费成功: 'Successful',
+        请上门自提: 'Successful',
+        等待付款: 'Successful',
+        '部分充值成功 退款成功': 'Successful',
+        卡密提取成功: 'Successful',
+        等待审核: 'Successful',
+        抢票中: 'Successful',
+        已取消: 'Cancelled',
+        订单取消: 'Cancelled',
+        抢票已取消: 'Cancelled',
+        出票失败: 'Failed',
+        未抢中: 'Failed',
+        已关闭: 'Failed',
+        '充值失败，退款成功': 'Refunded',
+        商品退库: 'Refunded',
+        退款完成: 'Refunded',
+        配送退货: 'Refunded',
+        失败退款: 'Refunded',
+    }
+
+    let tmstatus = {
+
+        交易成功: 'Successful',
+        出票成功: 'Successful',
+        物流派件中: 'Successful',
+        物流运输中: 'Successful',
+        快件已签收: 'Successful',
+        买家已付款: 'Successful',
+        订票成功: 'Successful',
+        卖家已发货: 'Successful',
+        预定成功: 'Successful',
+        已发奖: 'Successful',
+        快件已揽收: 'Successful',
+        已入住: 'Successful',
+        提交需求成功: 'Successful',
+        审核通过: 'Successful',
+        商家已确认: 'Successful',
+        部分发货: 'Successful',
+        线下支付: 'Successful',
+        卖家已确认: 'Successful',
+        拼团成功: 'Successful',
+        充值成功: 'Successful',
+        等待买家付款: 'Successful',
+        待付款: 'Successful',
+        已提交: 'Successful',
+        已取消: 'Cancelled',
+        交易关闭: 'Failed',
+        订票失败: 'Failed',
+        失效: 'Failed',
+        投保失败: 'Failed',
+        '超时未付款，订单已关闭': 'Failed',
+        申请已关闭: 'Failed',
+        未中奖: 'Failed',
+        审核未通过: 'Failed',
+        未付款: 'Failed',
+        已退款: 'Refunded',
+        '影院出票失败，已退款': 'Refunded'
+    }
+    let rst = { 0: jdstatus, 1: tmstatus };
+    return rst;
+}
+
+function getStatus(pfIdx, originStatus) {
+    return statusSet[pfIdx][originStatus];
 }
