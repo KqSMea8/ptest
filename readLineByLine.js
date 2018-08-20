ca18e006b6306c858b7a4ac5c5e1da6868384087
qiugh <graphy.qiu@gmail.com>
2018-08-20 13:16:47 +0800

    dd

---
+++ readLineByLine.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+let rl = require('readline');
+
+let filename = '';
+
+var lineReader = rl.createInterface({
+    input: fs.createReadStream(filename)
+});
+
+lineReader.on('line', function (line) {
+    dealwith(line);
+});
+
+
+lineReader.on('close', function () {
+
+});
+
+function dealwith(line) {
+
+}
