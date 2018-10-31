a8abedea286fe341825f3c1f0e78834841fcc26d
qiugh <graphy.qiu@gmail.com>
2018-10-31 15:33:18 +0800

    dd

--- puppeteer.js
+++ puppeteer.js
@@ -1,17 +1,30 @@
-const puppeteer = require('puppeteer');
-fs = require('fs')
-url = 'https://www.xiaohongshu.com/discovery/item/5bd57ddf07ef1c76a2a21214';
+let fs = require('fs');
+let cheerio = require('cheerio');
+let puppeteer = require('puppeteer');
 
+urls = fs.readFileSync('urls.txt').toString().trim().split('\n');
+len = urls.length
+start()
 
-(async () => {
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto(url);
-await page.waitFor(4000);
-
-await page.screenshot({path: 'example.png'});
-ret = await page.content()
-
-await fs.writeFile('tmp', ret)
-await browser.close();
-})();
+async function start() {
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+    for (let url of urls) {
+        let ret = await getUserId(url.trim(), page);
+        fs.appendFileSync('ret.url', ret + '\n');
+        len--;
+        console.log(len)
+    }
+    await browser.close();
+}
+async function getUserId(url, page) {
+    if (!url.match(/xiaohongshu/)) {
+        return " "
+    }
+    await page.goto(url);
+    await page.waitFor(4000);
+    ret = await page.content();
+    let $ = cheerio.load(ret);
+    let userId = $('.author-info').first().attr('href');
+    return userId;
+}
