f62b693a1526b71e8d442e59f44f99704dad6261
qiugh <graphy.qiu@gmail.com>
2018-12-03 20:41:53 +0800

    dd

--- import_pass_xhs.20000.js
+++ import_pass_xhs.20000.js
@@ -1,37 +1,45 @@
-
-
 let fs = require('fs');
 let MongoClient = require('mongodb').MongoClient;
 
 let mongodb;
+let docs = [];
+file = process.argv[2];
+fs.readFileSync(file).toString().trim().split('\n').forEach(function (line) {
+    let info = line.split('\t');
+    let doc = {};
+    doc['pid'] = info[0].trim();
+    doc['pname'] = info[1].trim();
+    doc['password'] = info[2].trim();
+    doc['xhsid'] = undefined;
+    doc['xhsname'] = undefined;
+    doc['xhsimg'] = undefined;
+    docs.push(doc);
+});
 
-MongoClient.connect('mongodb://127.0.0.1:27017/xhs').then(conn => {
+start();
+
+async function start() {
+    conn = await MongoClient.connect('mongodb://127.0.0.1:27017/xhs');
     mongodb = conn.db('xhs');
     mongodb.collection('user').createIndex({ pid: 1 });
     mongodb.collection('user').createIndex({ xhsid: 1 });
     mongodb.collection('user').createIndex({ xhsname: 1 });
+    await bulk(docs, 100, dealwith)
+    console.log('all done')
+}
+
+async function bulk(tasks, batch, handle) {
+    let step = Math.ceil(tasks.length / batch);
+    console.log(tasks.length, batch, step);
+    for (let i = 1; i <= step; i++) {
+        let tmp_tasks = tasks.slice((i - 1) * batch, i * batch);
+        console.log((i - 1) * batch, i * batch);
+        await handle(tmp_tasks);
+    }
+}
+
+function dealwith(docs) {
     let bulk = mongodb.collection('user').initializeUnorderedBulkOp();
-
-    fs.readFileSync('exists.20000').toString().trim().split('\n').forEach(function (line) {
-
-
-        let info = line.split('\t');
-        let doc = {};
-        doc['pid'] = info[0].trim();
-        doc['pname'] = info[1].trim();
-        doc['password'] = info[2].trim();
-        doc['xhsid'] = undefined;
-        doc['xhsname'] = undefined;
-        doc['xhsimg'] = undefined;
-        if (doc['xhsid']) {
-        }
-
-        bulk.find({
-            pid: doc.pid
-        }).upsert().update({ $setOnInsert: doc });
-
-    });
+    docs.forEach(doc => bulk.find({ pid: doc.pid }).upsert().update({ $setOnInsert: doc }));
     return bulk.execute();
-}).then(rst=>{
-    console.log('done',rst.length)
-});
+}
